6271dcfa09f403bfef51fc62073e4f0341812501
Kévin CHARNEAU <kevin.charneau85@gmail.com>
2020-02-09 11:08:41 +0100

    Refonte du site, mise à jour page accueil et About me

---
+++ aboutme.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>kcharneau.com</title>
+	<link rel="stylesheet" href="css/styleaboutme.css">
+	<link rel="stylesheet" href="css/menu.css">
+</head>
+
+<body>
+  <div class="showcase">
+    <header>
+      <div class="presentation">
+        <h1>ABOUT ME</h1>
+
+        <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+
+        <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+      </div>
+
+      <div class="photo_presentation">
+        <img src="icones/photo_presentation.png" width="280px">
+      </div>
+    </header>
+
+    <footer>
+      <div class="icones">
+  		  <div class="pinterest">
+  		 	  <a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
+  		  </div>
+
+  	    <div class="linkedin">
+  		    <a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="85em" /></a>
+  	    </div>
+
+  		  <div class="instagram">
+  		 	  <a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="85em" /></a>
+  		  </div>
+
+  		  <div class="d-flex">
+  		 	  <a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="85em" /></a>
+  		  </div>
+  	  </div>
+
+      <div class="mentions_legales"><a href="mentions_legales.html">Mentions légales</a>
+      </div>
+    </footer>
+
+    <nav>
+      <div class="menu-wrap">
+        <input type="checkbox" class="toggler">
+          <div class="hamburger"><div></div></div>
+      	    <div class="menu">
+        	    <div>
+          		  <div>
+            		  <ul>
+              		  <li><a href="home.php">Home</a></li>
+              		  <li><a href="aboutme.php">About Me</a></li>
+              		  <li><a href="cv.php">CV</a></li>
+              		  <li><a href="#">Formation</a></li>
+             			  <li><a href="#">Contact</a></li>
+            		  </ul>
+          		  </div>
+        		  </div>
+      		  </div>
+    	    </div>
+  	    </input>
+      </div>
+    </nav>
+  </div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>kcharneau.com</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/menu.css">
+</head>
+
+<header class="showcase">
+	<div class="container showcase-inner">
+  	<div class="icones">
+  		<div class="pinterest">
+  			<a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
+  		</div>
+
+  		<div class="linkedin">
+  			<a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="85em" /></a>
+  		</div>
+
+  		<div class="instagram">
+  			<a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="85em" /></a>
+  		</div>
+
+  		<div class="twitter">
+  			<a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="85em" /></a>
+  		</div>
+  	</div>
+
+    <div class="titre_principal">
+      <div class="prenom">
+        <h1>Kevin</h1>
+      </div>
+      <div class="nom">
+        <h1>Charneau</h1>
+      </div>
+      <div class="statut">
+        <h5>Etudiant informatique</h5>
+      </div>
+      <div class="etudes">
+        <h5>Dev/OPS</h5>
+      </div>
+    </div>
+
+    <div class="mentions_legales"><a href="mentions_legales.html">Mentions légales</a>
+    </div>
+  </div>
+</header>
+
+<nav>
+  <div class="menu-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger"><div></div></div>
+    <div class="menu">
+      <div>
+        <div>
+          <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="aboutme.php">About Me</a></li>
+            <li><a href="cv.php">CV</a></li>
+            <li><a href="#">Formation</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</html>
