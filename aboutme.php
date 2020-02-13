f72b9cc7fdede76837bfd25b570847d7f3db4307
Kévin CHARNEAU <kevin.charneau85@gmail.com>
2020-02-13 15:32:26 +0100

    Finalisation page accueil

--- aboutme.php
+++ aboutme.php
@@ -10,66 +10,70 @@
 </head>
 
 <body>
-  <div class="showcase">
-    <header>
-      <div class="presentation">
-        <h1>ABOUT ME</h1>
+  <header>
+    <nav>
+      <div class="menu-wrap">
+        <input type="checkbox" class="toggler">
+          <div class="hamburger"><div></div></div>
+            <div class="menu">
+              <div>
+                <div>
+                  <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="aboutme.php">About Me</a></li>
+                    <li><a href="icones/cv.pdf" target="_blank">CV</a></li>
+                    <li><a href="#">Projets</a></li>
+                    <li><a href="formation.php">Formation</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </input>
+      </div>
+    </nav>
+  </header>
 
-        <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+  <main>
+    <div class="presentation">
+      <h1>ABOUT ME</h1>
 
-        <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+      <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+
+      <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+
+      <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
       </div>
 
       <div class="photo_presentation">
         <img src="icones/photo_presentation.png" width="280px">
       </div>
-    </header>
+    </div>
+  </main>
 
-    <footer>
-      <div class="mentions_legales_bis"></div>
-      <div class="icones">
-  		  <div class="pinterest">
-  		 	  <a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
-  		  </div>
+  <footer>
+    <div class="mentions_legales_bis"></div>
+    <div class="icones">
+  		<div class="pinterest">
+  		 	<a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
+  		</div>
 
-  	    <div class="linkedin">
-  		    <a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="65em" /></a>
-  	    </div>
-
-  		  <div class="instagram">
-  		 	  <a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="65em" /></a>
-  		  </div>
-
-  		  <div class="twitter">
-  		 	  <a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="65em" /></a>
-  		  </div>
+  	  <div class="linkedin">
+  		  <a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="65em" /></a>
   	  </div>
 
-      <div class="mentions_legales"><a href="mentions_legales.html">Mentions légales</a>
-      </div>
-    </footer>
+  		<div class="instagram">
+  		 	<a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="65em" /></a>
+  		</div>
 
-    <nav>
-      <div class="menu-wrap">
-        <input type="checkbox" class="toggler">
-          <div class="hamburger"><div></div></div>
-      	    <div class="menu">
-        	    <div>
-          		  <div>
-            		  <ul>
-              		  <li><a href="home.php">Home</a></li>
-              		  <li><a href="aboutme.php">About Me</a></li>
-              		  <li><a href="icones/cv.pdf" target="_blank">CV</a></li>
-              		  <li><a href="#">Formation</a></li>
-             			  <li><a href="#">Contact</a></li>
-            		  </ul>
-          		  </div>
-        		  </div>
-      		  </div>
-    	    </div>
-  	    </input>
-      </div>
-    </nav>
-  </div>
+  		<div class="twitter">
+  		 	<a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="65em" /></a>
+  		</div>
+  	</div>
+
+    <div class="mentions_legales"> © Kévin Charneau 2020 <a href="mentions_legales.html"> Mentions légales</a>
+    </div>
+  </footer>
 </body>
 </html>
--- home.php
+++ home.php
@@ -10,62 +10,79 @@
 </head>
 
 <header class="showcase">
-	<div class="container showcase-inner">
-  	<div class="icones">
-  		<div class="pinterest">
-  			<a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
-  		</div>
-
-  		<div class="linkedin">
-  			<a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="65em" /></a>
-  		</div>
-
-  		<div class="instagram">
-  			<a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="65em" /></a>
-  		</div>
-
-  		<div class="twitter">
-  			<a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="65em" /></a>
-  		</div>
-  	</div>
-
-    <div class="titre_principal">
-      <div class="prenom">
-        <h1>Kevin</h1>
-      </div>
-      <div class="nom">
-        <h1>Charneau</h1>
-      </div>
-      <div class="statut">
-        <h5>Etudiant informatique</h5>
-      </div>
-      <div class="etudes">
-        <h5>Dev/OPS</h5>
+  <nav>
+    <div class="menu-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger"><div></div></div>
+      <div class="menu">
+        <div>
+          <div>
+            <ul>
+              <li><a href="home.php">Home</a></li>
+              <li><a href="aboutme.php">About Me</a></li>
+              <li><a href="icones/cv.pdf" target="_blank">CV</a></li>
+              <li><a href="#">Projets</a></li>
+              <li><a href="formation.php">Formation</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
+  </nav>
 
-    <div class="mentions_legales"><a href="mentions_legales.html">Mentions légales</a>
+  <div class="titre_principal">
+    <div class="prenom">
+      <h1>Kevin</h1>
+    </div>
+    <div class="nom">
+      <h1>Charneau</h1>
+    </div>
+    <div class="statut">
+      <h5>Etudiant informatique</h5>
+    </div>
+    <div class="etudes">
+      <h5>Dev/OPS</h5>
+    </div>
+
+    <div class="bouton_read_more">
+      <label for="read_more">Read More</label>
+      <input type="checkbox" id="read_more">
+        <div class="readmore">Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.
+
+        Quod opera consulta cogitabatur astute, ut hoc insidiarum genere Galli periret avunculus, ne eum ut praepotens acueret in fiduciam exitiosa coeptantem. verum navata est opera diligens hocque dilato Eusebius praepositus cubiculi missus est Cabillona aurum secum perferens, quo per turbulentos seditionum concitores occultius distributo et tumor consenuit militum et salus est in tuto locata praefecti. deinde cibo abunde perlato castra die praedicto sunt mota.<br />
+
+        Nec vox accusatoris ulla licet subditicii in his malorum quaerebatur acervis ut saltem specie tenus crimina praescriptis legum committerentur, quod aliquotiens fecere principes saevi: sed quicquid Caesaris implacabilitati sedisset, id velut fas iusque perpensum confestim urgebatur impleri.<br />
+
+        Nec vox accusatoris ulla licet subditicii in his malorum quaerebatur acervis ut saltem specie tenus crimina praescriptis legum committerentur, quod aliquotiens fecere principes saevi: sed quicquid Caesaris implacabilitati sedisset, id velut fas iusque perpensum confestim urgebatur impleri.<br />
+
+        Nec vox accusatoris ulla licet subditicii in his malorum quaerebatur acervis ut saltem specie tenus crimina praescriptis legum committerentur, quod aliquotiens fecere principes saevi: sed quicquid Caesaris implacabilitati sedisset, id velut fas iusque perpensum confestim urgebatur impleri.
+        </div>
     </div>
   </div>
 </header>
 
-<nav>
-  <div class="menu-wrap">
-    <input type="checkbox" class="toggler">
-    <div class="hamburger"><div></div></div>
-    <div class="menu">
-      <div>
-        <div>
-          <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="aboutme.php">About Me</a></li>
-            <li><a href="icones/cv.pdf" target="_blank">CV</a></li>
-            <li><a href="#">Formation</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </div>
-      </div>
+<footer>
+  <div class="mentions_legales_bis"></div>
+  <div class="icones">
+    <div class="pinterest">
+      <a href="https://www.pinterest.com" target="_blank" title="pinterest.com"><img src="icones/pinterest.png" alt="Pinterest" width="65em" /></a>
+    </div>
+
+    <div class="linkedin">
+      <a href="https://www.linkedin.com/in/k%C3%A9vin-charneau-4972b5183/" target="_blank" title="linkedin.com"><img src="icones/linkedin1.png" alt="Linkedin" width="65em" /></a>
+    </div>
+
+    <div class="instagram">
+      <a href="https://www.instagram.com" target="_blank" title="instagram.com"><img src="icones/insta.png" alt="Instagram" width="65em" /></a>
+    </div>
+
+    <div class="twitter">
+      <a href="https://www.twitter.com" target="_blank" title="twitter.com"><img src="icones/twitter.png" alt="Twitter" width="65em" /></a>
     </div>
   </div>
-</nav>
+
+  <div class="mentions_legales"> © Kévin Charneau 2020 <a href="mentions_legales.html">Mentions légales</a>
+  </div>
+</footer>
 </html>
